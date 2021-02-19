630940ad20fa420050a48eb0418e18d56652c8da
Uspinhal <uspinhal@yahoo.com>
2021-02-18 22:18:17 -0300

    Implementing DepartmentDao & SellerDao Interfaces

---
+++ jdbc/src/model/DAO/DepartmentDAO.java
@@ -0,0 +1,14 @@
+package model.DAO;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDAO {
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteByID(Integer id);
+    Department findByID(Integer id);
+    List<Department> findAll();
+    
+}
